0df83c59f37764529359764c09d35d5a19c64ebc
abra <lukaabramovic2@gmail.com>
2022-08-24 08:20:34 +0200

    Adds fixes

--- src/areas/area.module.ts
+++ src/areas/area.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaService } from '../prisma.service';
 import { AreaController } from './area.controller';
 import { AreaService } from './area.service';
 
 @Module({
   controllers: [AreaController],
-  providers: [AreaService],
+  providers: [AreaService, PrismaService],
   exports: [AreaService],
 })
 export class AreaModule {}
--- src/locations/location.module.ts
+++ src/locations/location.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaService } from '../prisma.service';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
 
 @Module({
   controllers: [LocationController],
-  providers: [LocationService],
+  providers: [LocationService, PrismaService],
   exports: [LocationService],
 })
 export class LocationModule {}
