d96fdebfc5232a7f87b19b959feffae14def5d5a
abra <lukaabramovic2@gmail.com>
2022-10-31 16:38:01 +0100

    Removes previous project idea data and leaves in the bear-bones for the NestJS and Prisma API

--- src/data/server/auth/auth.service.ts
+++ src/data/server/auth/auth.service.ts
@@ -23,7 +23,6 @@ export class AuthService {
   }
 
   async register(signUpPayload: SignUp): Promise<User | null> {
-    // try {
     const hashedPassword = await bcrypt.hash(signUpPayload.password, 10);
 
     const user = await this.userService.create({
@@ -33,10 +32,6 @@ export class AuthService {
     delete user.password;
 
     return user;
-    // } catch (error: unknown) {
-    //   console.log(error, 'errrrr');
-    //   throw error;
-    // }
   }
 
   async login(loginPayload: LoginRequest): Promise<LoginResponse> {
